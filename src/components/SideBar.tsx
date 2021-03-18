44c49fdadc69ce093ca3da52d8208d4d34b70991
Algacyr Melo <algacyr.junior@gmail.com>
2021-03-18 12:38:29 -0300

    :tada: watchme_components created

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { GenreResponseProps } from "../App";
+
+
+interface SidebarProps {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+}
+
+export function SideBar(props: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
